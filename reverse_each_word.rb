6107cdf58a58e3cab67a1ef48a17d187282ee98c
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-09 05:47:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,8 @@
-#def reverse_each_word(array)
- # array.each(array.reverse)
-  #array
-#end  
-
 def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+  array = string.split(" ")
   new_array = []
-  array.collect do|string| #iterate over the array
-    new_array << string.reverse #reverse each word in the array
+  array.collect do|string|
+    new_array << string.reverse
   end
   new_array.join(" ")
 end
